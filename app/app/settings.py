021136e37d9375ac34c678ce0e88e07869ce6dee
Enn Petersoo <agentic@agentic.ee>
2024-11-15 12:56:26 +0200

    first test

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASS", 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_SCHEMA_CLASS", 'drf_spectacular.openapi.AutoSchema'
 }
